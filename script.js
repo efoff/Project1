ff7b1a81054235a381383717b7616178c9d8c3df
efoff <efoff2300@gmail.com>
2021-08-30 15:27:45 +0530

    First working version

---
+++ script.js
@@ -0,0 +1,53 @@
+const btn = document.querySelector("button");
+let tasks = [];
+const renderTable = () => {
+    
+    var itterator = 1;
+    var table = document.getElementById("dyn");
+    table.innerHTML = '';
+    tasks.forEach((ele) => {
+        const newRow = document.createElement("tr");
+        for(let i = -1;i<2;i++)
+        {
+            const head = document.createElement("td");
+            let data;
+            if(!(i<0))
+            data = document.createTextNode(ele[i]);
+            else
+            data = document.createTextNode(itterator);
+            head.appendChild(data);
+            newRow.appendChild(head);
+        }
+        const btn = document.createElement("button");
+        btn.setAttribute("onClick",`add(${itterator})`);
+        btn.innerHTML += "Remove";
+        newRow.appendChild(btn);
+        table.appendChild(newRow);
+        // console.log(ele.Id,ele.TaskName,ele.Time)
+        // const data1 = document.createTextNode(ele.Id.toString());
+        // const data2 = document.createTextNode(ele.TaskName.toString());
+        // const data3 = document.createTextNode(ele.Time.toString());
+        // head.appendChild(data);
+        // newRow.appendChild(head);
+        // table.appendChild(newRow);
+        itterator++;
+    })
+}
+renderTable();
+
+const add = (Flag) => {
+
+    if(Flag<0){
+    console.log("we here")
+    var TaskName = document.getElementById("Task");
+    var TaskTime = document.getElementById("Time");
+    tasks.push([TaskName.value,TaskTime.value]);
+    TaskName.value = "";
+    TaskTime.value="";
+    renderTable();
+    }
+    else{
+        tasks.splice(Flag-1,1);
+        renderTable();
+    }
+}
